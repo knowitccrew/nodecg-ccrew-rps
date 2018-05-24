d1305ae68e04be2dd780e6a9a446d4522568ac0e
S. Christoffer Eliesen <christoffer@eliesen.no>
2018-05-25 00:30:18 +0200

    game.js: More progress.

--- extension/game.js
+++ extension/game.js
@@ -2,12 +2,50 @@
 
 module.exports = function (nodecg) {
 
+  const rounds = 7;
+  const gamep1 = nodecg.Replicant('rps_game_p1', { defaultValue: [] });
+  const gamep2 = nodecg.Replicant('rps_game_p2', { defaultValue: [] });
+
+  const stringToValue = {
+    'true': true,
+    't': true,
+    '1': true,
+    1: true,
+    'yes': true,
+
+    'false': false,
+    'f': false,
+    '0': false,
+    0: false,
+    'no': false,
+  };
+
   nodecg.listenFor('setp1', (data) => {
-    console.log(`Got setp1: "${data}"`);
+    const value = stringToValue[data && data.toLowerCase()];
+    if (value != null) {
+      console.log(`Got valid setp1: "${data}" (${value})`);
+      if (gamep1.value.length < rounds) {
+        gamep1.value.push(value);
+      } else {
+        console.log("ERROR: Game for player 1 is full!", gamep1.value.length, gamep1.value);
+      }
+    } else {
+      console.log(`Got INVALID setp1: "${data}"`);
+    }
   });
 
   nodecg.listenFor('setp2', (data) => {
-    console.log(`Got setp2: "${data}"`);
+    const value = stringToValue[data && data.toLowerCase()];
+    if (value != null) {
+      console.log(`Got valid setp2: "${data}" (${value})`);
+      if (gamep2.value.length < rounds) {
+        gamep2.value.push(value);
+      } else {
+        console.log("ERROR: Game for player 2 is full!", gamep2.value.length, gamep2.value);
+      }
+    } else {
+      console.log(`Got INVALID setp2: "${data}"`);
+    }
   });
 
   nodecg.listenFor('finalize', (data) => {
@@ -15,4 +53,3 @@ module.exports = function (nodecg) {
   });
 
 };
-
