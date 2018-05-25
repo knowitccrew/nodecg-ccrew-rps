6a85026a19595bdd78ab923d096ace408022d547
S. Christoffer Eliesen <christoffer@eliesen.no>
2018-05-25 03:00:39 +0200

    api,game_lib: More api.

--- extension/api.js
+++ extension/api.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const app = express();
 
-const { reset_game } = require('./game_lib');
+const { finalize, player_hand, reset_game, set_round_win } = require('./game_lib');
 
 const ns = 'nodecg-ccrew-rps';
 
 module.exports = function (nodecg) {
   const game = nodecg.Replicant('rps_game', { defaultValue: [] });
+  const hands = nodecg.Replicant('rps_hands', { defaultValue: { p1: null, p2: null } });
 
   app.get(`/${ns}/api/reset_game`, (req, res) => {
     reset_game({ game });
+    res.send('OK!');
+  });
+
+  app.get(`/${ns}/api/set_round_win`, (req, res) => {
+    const { data } = req.query;
+    set_round_win({ data, game });
+    res.send('OK!');
+  });
+
+  app.get(`/${ns}/api/player1_hand`, (req, res) => {
+    const { data } = req.query;
+    player_hand({ player: 1, data, hands });
+    res.send('OK!');
+  });
+
+  app.get(`/${ns}/api/player2_hand`, (req, res) => {
+    const { data } = req.query;
+    player_hand({ player: 2, data, hands });
+    res.send('OK!');
+  });
+
+  app.get(`/${ns}/api/finalize`, (req, res) => {
+    finalize({ hands, game });
     res.send('OK!');
   });
 
--- extension/game_lib.js
+++ extension/game_lib.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const rounds = 7;
+
 const set_round_win = ({ data, game }) => {
   console.log(`Got set_round_win: "${data}"`);
   const newGame = game.value.slice();
@@ -25,8 +27,76 @@ const reset_game = ({ game }) => {
   game.value = [];
 };
 
+const possibleHands = ['rock', 'paper', 'scissors'];
+
+const player_hand = ({ player, data, hands }) => {
+  if (!possibleHands.includes(data)) {
+    console.error(`Got bad hand? "${data}"`);
+    return;
+  }
+
+  let { p1, p2 } = hands.value;
+  if (player === 1) {
+    p1 = data;
+  } else if (player === 2) {
+    p2 = data;
+  } else {
+    console.error(`Got bad player? "${player}"`);
+    return;
+  }
+
+  console.log(`Player ${player} hand: "${data}"`);
+  hands.value = { p1, p2 };
+};
+
+const winCalc = {
+  'rock_rock': null,
+  'rock_paper': 2,
+  'rock_scissors': 1,
+
+  'paper_rock': 1,
+  'paper_paper': null,
+  'paper_scissors': 2,
+
+  'scissors_rock': 2,
+  'scissors_paper': 1,
+  'scissors_scissors': null,
+}
+
+const finalize = ({ hands, game }) => {
+  const { p1, p2 } = hands.value;
+  if (!p1 || !p2) {
+    console.log("Can't finalize without two good hands:", hands.value);
+    return;
+  }
+
+  const winner = winCalc[`${p1}_${p2}`];
+  if (typeof winner === 'undefined') {
+    console.error("Can't calculate winner! hands:", hands.value);
+
+  } else if (winner === null) {
+    console.log(hands.value, "tie!");
+
+  } else if (winner === 1) {
+    console.log(hands.value, "Player one wins!");
+    set_round_win({ data: 1, game });
+    hands.value = { p1: null, p2: null };
+
+  } else if (winner === 2) {
+    console.log(hands.value, "Player two wins!");
+    set_round_win({ data: 2, game });
+    hands.value = { p1: null, p2: null };
+
+  } else {
+    // not supposed to happen:
+    console.error("This makes no sense, can't calculate winner! hands:", hands.value);
+  }
+
+};
 
 module.exports = {
-  set_round_win,
+  finalize,
+  player_hand,
   reset_game,
+  set_round_win,
 };
